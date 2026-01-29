83bb0943bbd167a563c4087dc3e9078ff96c5fe8
GiuliaSimoncini <giulia.simoncini2@edu.unifi.it>
2026-01-29 10:40:19 +0100

    Small changes

--- calcolo_tempi_esecuzione.py
+++ calcolo_tempi_esecuzione.py
@@ -113,7 +113,7 @@ def plot_barchart(name_csv_file):
 
     plt.xlabel('Tempo di Esecuzione in secondi', fontsize=13)
     plt.ylabel('Nome del Benchmark', fontsize=13)
-    plt.title('Calcolo dei tempi di esecuzione dei vari benchamark in TACLeBench', fontsize=16)
+    plt.title('Calcolo dei tempi di esecuzione dei vari benchmark in TACLeBench', fontsize=16)
     
     # Si aggiunge una griglia verticale
     plt.grid(axis='x', linestyle='--', alpha=0.6)
